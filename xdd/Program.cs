28894ad77f9f1593b63963cd9b97a8a4c319d41b
Leon Johannesson <leonj@Leons-MBP.lan>
2022-08-08 01:58:44 +0200

    initial commit

---
+++ xdd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+class Program {
+    public static void Main() {
+        string[] prefixes = { "http://*:8001/" };
+
+
+        if (prefixes == null || prefixes.Length == 0)
+        throw new ArgumentException("prefixes");
+
+        // Create a listener.
+        HttpListener listener = new HttpListener();
+        // Add the prefixes.
+        foreach (string s in prefixes)
+        {
+            listener.Prefixes.Add(s);
+        }
+        listener.Start();
+        Console.WriteLine("Listening...");
+
+        // Note: The GetContext method blocks while waiting for a request.
+        HttpListenerContext context = listener.GetContext();
+        HttpListenerRequest request = context.Request;
+
+
+        // Obtain a response object.
+        HttpListenerResponse response = context.Response;
+
+        // Construct a response.
+        string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+        // Get a response stream and write the response to it.
+        response.ContentLength64 = buffer.Length;
+        System.IO.Stream output = response.OutputStream;
+        output.Write(buffer,0,buffer.Length);
+        // You must close the output stream.
+        output.Close();
+        listener.Stop();
+    }
+}
